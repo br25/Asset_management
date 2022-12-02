f557acabf2756a0278668f3dbba463dfff3bf0ba
br25 <md.ashrafulislamrobin12345@gmail.com>
2022-12-02 20:21:59 +0600

    update asset model

--- assets/models.py
+++ assets/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from companies.models import Company
 from employees.models import Employee
-from core.enums import AssetsType, Status, HandoverStatus, ConditionStatus
+from core.enums import AssetsType, Status, Condition
 
 
 class Asset(models.Model):
@@ -11,14 +11,15 @@ class Asset(models.Model):
     types = models.CharField(max_length=255, choices=AssetsType.choices)
     owner = models.ForeignKey(
         Company, related_name='company', on_delete=models.CASCADE)
-    status = models.CharField(max_length=255, choices=Status.choices)
+    status = models.CharField(max_length=255, choices=Status.choices, default=Status.NEW)
     is_assigned = models.BooleanField(default=False)
 
 
-class AssetDeligation(models.Model):
-    handover_at = models.DateTimeField()
-    handover_type = models.CharField(max_length=255, choices=HandoverStatus.choices)
+class AssetDelegation(models.Model):
+    given_at = models.DateField(auto_now_add=True)
+    expire_at = models.DateField(blank=True, null=True)
     reason = models.TextField(max_length=255)
-    condition = models.CharField(max_length=255, choices=ConditionStatus.choices)
+    condition = models.CharField(max_length=255, choices=Condition.choices)
     asset = models.ForeignKey(Asset, related_name="asset", on_delete=models.CASCADE)
     employee = models.ForeignKey(Employee, related_name="employee_asset", on_delete=models.SET_NULL, null=True)
+    is_returned = models.BooleanField(default=False)
